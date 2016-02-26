fb91581b121fa78a3f4a9ea12b6486c72e8e675c
jackforyou <jackfor_you@hotmail.com>
2016-02-26 12:48:31 +0700

    Update urls.py

--- django_projects/Project/Project/urls.py
+++ django_projects/Project/Project/urls.py
@@ -1,16 +1,24 @@
-from django.conf.urls import patterns, include, url
+# from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
+# from django.contrib import admin
+# from django.conf.urls import include, url
+# from django.views.debug import default_urlconf
+# admin.autodiscover()
+
+# urlpatterns = patterns('',
+#     # Examples:
+#     # url(r'^$', 'Project.views.home', name='home'),
+#     # url(r'^blog/', include('blog.urls')),
+
+#     url(r'^admin/', include(admin.site.urls)),
+#     url(r'^', include('caterlist.urls')),
+#     # url(r'^$', default_urlconf),
+# )
+
 from django.conf.urls import include, url
-from django.views.debug import default_urlconf
-admin.autodiscover()
+from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('caterlist.urls')),
-    # url(r'^$', default_urlconf),
-)
+urlpatterns = [
+	url(r'^', include('caterlist.urls')),
+    url(r'^admin/', admin.site.urls),
+]
